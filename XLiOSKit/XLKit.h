e87592173844e19efdf8b8c97e46514a21a7352a
Martin Barreto <martin@xmartlabs.com>
2014-09-24 14:24:00 -0300

    System Versioning Preprocessor Macros

--- XLiOSKit/XLKit.h
+++ XLiOSKit/XLKit.h
@@ -70,6 +70,25 @@
 
 #endif
 
+#ifndef SYSTEM_VERSION_EQUAL_TO
+#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
+#endif
+
+#ifndef SYSTEM_VERSION_GREATER_THAN
+#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
+#endif
+
+#ifndef SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO
+#   define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
+#endif
+
+#ifndef SYSTEM_VERSION_LESS_THAN
+#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
+#endif
+
+#ifndef SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO
+#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)
+#endif
 
 
 @interface XLKit : NSObject
